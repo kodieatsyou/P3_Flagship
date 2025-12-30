ec1ab75f005c4154cc1e7bb1530b998fcea54243
Tanner <tannerjmay@gmail.com>
2025-12-29 18:25:55 -0700

    Cleaned up unneeded config

--- Plugins/TacticsCore/Source/TacticsCore/TacticsCore.Build.cs
+++ Plugins/TacticsCore/Source/TacticsCore/TacticsCore.Build.cs
@@ -6,48 +6,17 @@ public class TacticsCore : ModuleRules
 {
 	public TacticsCore(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		// Keep this module engine-agnostic as long as possible.
+		// Only add CoreUObject/Engine when you have a concrete need.
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+		});
 	}
 }
